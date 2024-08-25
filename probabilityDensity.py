28f29011da41e88c2f3366a2f8d5cb4f3f6bf644
Vishal Verma <112986978+visha1v@users.noreply.github.com>
2024-08-26 02:54:32 +0530

    Update probabilityDensity.py

--- probabilityDensity.py
+++ probabilityDensity.py
@@ -1,48 +1,45 @@
-import math  # Importing
-import numpy as np  # Necessary
-import matplotlib.pyplot as plt  # Libraries
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Taking R2/y constant.
-y = float(input("What is the value of R2?"))
-r1 = float(input("What is the start value of R1?"))
-r2 = float(input("What is the end value of R1?"))
-c = float(input("What is the step value of R1?"))
+y = float(input("What is the value of R2? "))
+r1 = float(input("What is the start value of R1? "))
+r2 = float(input("What is the end value of R1? "))
+c = float(input("What is the step value of R1? "))
 
 # Range for R1/x values.
 X = np.arange(r1, r2, c)
+n = len(X)
 
-A = np.arange(r1, r2, c / 2)  # psi(1,2) values for different R1 and R2
-B = np.arange(r1, r2, c / 2)  # psi(2,1) values for different R1 and R2
-p = len(A) - 1
+# Precompute constant values for efficiency
+const_factor = (8 / 3.14) * (1 / 3.14)
 
-for ind, x in enumerate(X):
-    k = p - ind - 1
-    A[k] = float((8 / 3.14) * (math.exp(-4 * x)) * (1 / 3.14) * (1 - 2 * y + y * y) * (math.exp(-2 * y)))
-    B[k] = float((8 / 3.14) * (math.exp(-4 * y)) * (1 / 3.14) * (1 - 2 * x + x * x) * (math.exp(-2 * x)))
-for ind, x in enumerate(X):
-    k = p + ind
-    A[k] = float((8 / 3.14) * (math.exp(-4 * x)) * (1 / 3.14) * (1 - 2 * y + y * y) * (math.exp(-2 * y)))
-    B[k] = float((8 / 3.14) * (math.exp(-4 * y)) * (1 / 3.14) * (1 - 2 * x + x * x) * (math.exp(-2 * x)))
+# Pre-allocate arrays for psi values
+A = np.zeros(n)
+B = np.zeros(n)
 
-########################################################################################################
-S = np.arange(r1, r2, c / 2)  # Symmetric Solution
-AS = np.arange(r1, r2, c / 2)  # Anti-Symmetric Solution
+# Calculate psi(1,2) and psi(2,1) values for R1 and R2
+for ind, x in enumerate(X):
+    exp_neg_4x = math.exp(-4 * x)
+    exp_neg_2y = math.exp(-2 * y)
+    exp_neg_4y = math.exp(-4 * y)
+    exp_neg_2x = math.exp(-2 * x)
+    
+    A[ind] = const_factor * exp_neg_4x * (1 - 2 * y + y**2) * exp_neg_2y
+    B[ind] = const_factor * exp_neg_4y * (1 - 2 * x + x**2) * exp_neg_2x
 
-########################################################################################################
-for ind, x in enumerate(A):
-    S[ind] = ((A[ind] + B[ind]) ** 2) / 2
-    AS[ind] = ((A[ind] - B[ind]) ** 2) / 2
-J = (r1, r2, 2 * c)
-for ind, x in enumerate(X):
-    k = p + ind - 1
-    J[k] = (-(X[ind] - y))
-for ind, x in enumerate(X):
-    J[k] = (X[ind] - y)
+# Calculate Symmetric and Anti-Symmetric solutions
+S = ((A + B) ** 2) / 2
+AS = ((A - B) ** 2) / 2
+
+# Calculate J values (|R1 - R2|)
+J = np.abs(X - y)
 
 ####################################.........Graph Plotting........#########################################
 fig, ax1 = plt.subplots()
 ax1.plot(J, S, 'b-')
-ax1.set_xlabel('|R1-R2|')
+ax1.set_xlabel('|R1 - R2|')
 ax1.set_ylabel('Symmetric Probability Distribution', color='b')
 ax1.tick_params('y', colors='b')
 
@@ -53,4 +50,3 @@ ax2.tick_params('y', colors='r')
 
 fig.tight_layout()
 plt.show()
-
