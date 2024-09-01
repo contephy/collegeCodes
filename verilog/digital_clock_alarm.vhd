3ae8258bfdeb836401cb981be4f252ab60a5f536
visha1v <vishalverma@tutanota.com>
2024-09-01 15:53:04 +0530

    udpate

--- verilog/digital_clock_alarm.vhd
+++ verilog/digital_clock_alarm.vhd
@@ -1,7 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity Digital_Clock is
     Port (
@@ -14,33 +13,58 @@ entity Digital_Clock is
 end Digital_Clock;
 
 architecture Behavioral of Digital_Clock is
-    signal hours   : INTEGER := 0;
-    signal minutes : INTEGER := 0;
-    signal seconds : INTEGER := 0;
+    signal hours   : unsigned(7 downto 0) := (others => '0');
+    signal minutes : unsigned(7 downto 0) := (others => '0');
+    signal seconds : unsigned(7 downto 0) := (others => '0');
     signal alarm_time : STD_LOGIC_VECTOR (23 downto 0) := (others => '0');
+    signal one_sec_pulse : STD_LOGIC := '0';
 begin
+    process (clk, reset)
+        variable sec_count : unsigned(24 downto 0) := (others => '0');
+    begin
+        if reset = '1' then
+            sec_count := (others => '0');
+            one_sec_pulse <= '0';
+        elsif rising_edge(clk) then
+            if sec_count = 49999999 then
+                sec_count := (others => '0');
+                one_sec_pulse <= '1';
+            else
+                sec_count := sec_count + 1;
+                one_sec_pulse <= '0';
+            end if;
+        end if;
+    end process;
+
+-- Timekeeping process
     process (clk, reset)
     begin
         if reset = '1' then
-            hours   <= 0;
-            minutes <= 0;
-            seconds <= 0;
+            hours   <= (others => '0');
+            minutes <= (others => '0');
+            seconds <= (others => '0');
         elsif rising_edge(clk) then
-            seconds <= seconds + 1;
-            if seconds = 60 then
-                seconds <= 0;
-                minutes <= minutes + 1;
-                if minutes = 60 then
-                    minutes <= 0;
-                    hours <= hours + 1;
-                    if hours = 24 then
-                        hours <= 0;
+            if one_sec_pulse = '1' then
+                if seconds = 59 then
+                    seconds <= (others => '0');
+                    if minutes = 59 then
+                        minutes <= (others => '0');
+                        if hours = 23 then
+                            hours <= (others => '0');
+                        else
+                            hours <= hours + 1;
+                        end if;
+                    else
+                        minutes <= minutes + 1;
                     end if;
+                else
+                    seconds <= seconds + 1;
                 end if;
             end if;
         end if;
     end process;
 
+-- Alarm process
     process (current_time)
     begin
         if current_time = alarm_time then
@@ -50,5 +74,5 @@ begin
         end if;
     end process;
 
-    current_time <= STD_LOGIC_VECTOR(to_unsigned(hours, 8) & to_unsigned(minutes, 8) & to_unsigned(seconds, 8));
+    current_time <= std_logic_vector(hours & minutes & seconds);
 end Behavioral;
--- verilog/traffic_light_controller.vhd
+++ verilog/traffic_light_controller.vhd
@@ -1,86 +1,60 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity Traffic_Light is
     Port (
         clk       : in STD_LOGIC;
         reset     : in STD_LOGIC;
-        lights    : out STD_LOGIC_VECTOR (2 downto 0)  -- Red, Yellow, Green
+        lights    : out STD_LOGIC_VECTOR (2 downto 0)
     );
 end Traffic_Light;
 
 architecture Behavioral of Traffic_Light is
     type state_type is (RED, GREEN, YELLOW);
-    signal state, next_state : state_type;
-    signal timer : INTEGER := 0;
+    signal state : state_type := RED;
+    signal counter : INTEGER := 0;
 
-    -- Constants for timing (e.g., in clock cycles)
-    constant RED_TIME : INTEGER := 50000000;  -- 50 million clock cycles
-    constant GREEN_TIME : INTEGER := 50000000;  -- 50 million clock cycles
-    constant YELLOW_TIME : INTEGER := 10000000;  -- 10 million clock cycles
+    constant RED_TIME : INTEGER := 50000000;   
+    constant GREEN_TIME : INTEGER := 50000000; 
+    constant YELLOW_TIME : INTEGER := 10000000; 
 
 begin
-    -- FSM process
     process (clk, reset)
     begin
         if reset = '1' then
             state <= RED;
-            timer <= 0;
+            counter <= 0;
         elsif rising_edge(clk) then
-            state <= next_state;
-            if timer = 0 then
-                timer <= timer + 1;
-            else
-                timer <= timer + 1;
-            end if;
+            case state is
+                when RED =>
+                    if counter = RED_TIME then
+                        state <= GREEN;
+                        counter <= 0;
+                    else
+                        counter <= counter + 1;
+                    end if;
+                when GREEN =>
+                    if counter = GREEN_TIME then
+                        state <= YELLOW;
+                        counter <= 0;
+                    else
+                        counter <= counter + 1;
+                    end if;
+                when YELLOW =>
+                    if counter = YELLOW_TIME then
+                        state <= RED;
+                        counter <= 0;
+                    else
+                        counter <= counter + 1;
+                    end if;
+            end case;
         end if;
     end process;
 
-    -- Next state logic
-    process (state, timer)
-    begin
-        case state is
-            when RED =>
-                if timer = RED_TIME then
-                    next_state <= GREEN;
-                    timer <= 0;
-                else
-                    next_state <= RED;
-                end if;
-            when GREEN =>
-                if timer = GREEN_TIME then
-                    next_state <= YELLOW;
-                    timer <= 0;
-                else
-                    next_state <= GREEN;
-                end if;
-            when YELLOW =>
-                if timer = YELLOW_TIME then
-                    next_state <= RED;
-                    timer <= 0;
-                else
-                    next_state <= YELLOW;
-                end if;
-            when others =>
-                next_state <= RED;
-        end case;
-    end process;
-
-    -- Output logic
-    process (state)
-    begin
-        case state is
-            when RED =>
-                lights <= "100";  -- Red light on
-            when GREEN =>
-                lights <= "001";  -- Green light on
-            when YELLOW =>
-                lights <= "010";  -- Yellow light on
-            when others =>
-                lights <= "000";  -- Default (all lights off)
-        end case;
-    end process;
+    lights <= "100" when state = RED else
+              "001" when state = GREEN else
+              "010" when state = YELLOW else
+              "000";
 
 end Behavioral;
