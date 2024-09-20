4325227b3a9563b63853b29e8e886d416983dbb9
Vishal Verma <112986978+visha1v@users.noreply.github.com>
2024-09-20 15:08:56 +0530

    Update rlc_circuit_simulation.m

--- matlab/rlc_circuit_simulation.m
+++ matlab/rlc_circuit_simulation.m
@@ -1,5 +1,4 @@
 function RLCcircuitSimulation
-    % Parameters
     R = 1000;   % resistance (ohms)
     L = 0.1;    % inductance (H)
     C = 1e-4;   % capacitance (F)
@@ -8,13 +7,10 @@ function RLCcircuitSimulation
     % Differential equation
     odefun = @(t, y) [y(2); (V0 - y(2)*R - y(1)/C) / L];
     
-    % Initial conditions
     y0 = [0; 0]; % initial capacitor voltage and inductor current
     
-    % Time span
     tspan = [0, 0.01];
     
-    % Solve ODE
     [t, y] = ode45(odefun, tspan, y0);
     
     % Plot results
