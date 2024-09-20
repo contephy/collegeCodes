2a9d1ee6f78114c593236f2ab27c0a86372652c9
Vishal Verma <112986978+visha1v@users.noreply.github.com>
2024-09-20 15:11:08 +0530

    Update harmonic_oscillator_simulation.m

--- matlab/harmonic_oscillator_simulation.m
+++ matlab/harmonic_oscillator_simulation.m
@@ -1,20 +1,15 @@
 function harmonicOscillator
-    % Parameters
     m = 1;      % mass (kg)
     k = 10;     % spring constant (N/m)
     b = 0.5;    % damping coefficient (Ns/m)
     F = @(t) 0; % external force (N)
     
-    % Differential equation
     odefun = @(t, y) [y(2); (F(t) - b * y(2) - k * y(1)) / m];
-    
-    % Initial conditions
+
     y0 = [1; 0]; % initial displacement and velocity
     
-    % Time span
     tspan = [0, 10];
     
-    % Solve ODE
     [t, y] = ode45(odefun, tspan, y0);
     
     % Plot results
