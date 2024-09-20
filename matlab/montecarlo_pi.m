e7a96cca5facd6b9b0b988a97c13f3593ea16861
Vishal Verma <112986978+visha1v@users.noreply.github.com>
2024-09-20 15:10:21 +0530

    Update montecarlo_pi.m

--- matlab/montecarlo_pi.m
+++ matlab/montecarlo_pi.m
@@ -1,23 +1,17 @@
-% Number of random points
 N = 10000;
 
-% Generate random points (x, y) within the unit square
 x = rand(N, 1);
 y = rand(N, 1);
 
-% Calculate the distance of each point from the origin (0, 0)
 distances = sqrt(x.^2 + y.^2);
 
-% Count the number of points inside the quarter circle (distance <= 1)
 inside_circle = sum(distances <= 1);
 
-% Estimate pi using the ratio of points inside the quarter circle
 pi_estimate = 4 * inside_circle / N;
 
-% Display the estimated value of pi
 disp(['Estimated value of Pi: ', num2str(pi_estimate)])
 
-% Plot the points
+% Plot
 figure;
 hold on;
 axis square;
@@ -25,7 +19,7 @@ axis square;
 plot(x(distances <= 1), y(distances <= 1), 'b.');
 % Points outside the circle
 plot(x(distances > 1), y(distances > 1), 'r.');
-% Plot the quarter circle
+
 theta = linspace(0, pi/2, 100);
 plot(cos(theta), sin(theta), 'k-', 'LineWidth', 2);
 title(['Monte Carlo Simulation for Pi, N = ', num2str(N)]);
